f020cf6af617274cc175e0d9bf21a9402b299ee9
Ekaterina Gornostaewa <pvivian@oa-e1.msk.21-school.ru>
2021-03-03 18:53:42 +0300

    List: assign, erase, insert

--- containers/list.hpp
+++ containers/list.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   list.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pvivian <pvivian@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: pvivian <pvivian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 11:57:56 by pvivian           #+#    #+#             */
-/*   Updated: 2021/03/02 18:59:09 by pvivian          ###   ########.fr       */
+/*   Updated: 2021/03/03 18:44:53 by pvivian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "list_iterator.hpp"
 # include <limits>
 
-#include <iostream> // delete
+#include <iostream> // delete it
 
 namespace ft
 {
@@ -56,41 +56,61 @@ namespace ft
 		
 
 	// *************** Additional functions ***************
+		void create_list_end(void)
+		{
+			this->tail = new node;
+			this->head = this->tail;
+			this->tail->value = 0;
+			this->tail->prev = this->head;
+			this->tail->next = this->head;
+			this->list_size = 0;
+			return; 
+		}
+		
 		void create_list_head(const value_type& val)
 		{
-			head = new node;
-			head->value = val;
-			head->prev = nullptr;
-			head->next = nullptr;
-			tail = head;
-			list_size = 1;
+			this->head = new node;
+			this->head->value = val;
+			this->head->prev = this->tail;
+			this->head->next = this->tail;
+			this->tail->prev = this->head;
+			this->tail->next = this->head;
+			this->list_size++;
+			this->tail->value = list_size;
 			return; 
 		}
 		
 	public:
 	// *************** Constructors ***************
-		explicit list (void) : head(nullptr), tail(nullptr), list_size(0)
+		explicit list (void)
 		{ 
-			// create_list_head(nullptr);
+			create_list_end();
 			return; 
 		}
 		
 		explicit list (size_type n, const value_type& val = value_type())
 		{
-			create_list_head(val);
-			for (size_type i = 1; i < n; i++)
-				push_back(val);
+			create_list_end();
+			for (size_type i = 0; i < n; i++)
+				push_front(val);
 			return;
 		}
 		
 		// template <class InputIterator>
-		// list (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+		// // list (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+		// list (InputIterator first, InputIterator last)
+		// {
+		// 	create_list_end();
+		// 	insert(first, last);
+		// 	return;
+		// }
 		
 		list (const list& x)
 		{
 			if (this != &x)
 			{
 				iterator it = x.begin();
+				create_list_end();
 				create_list_head(it.ptr->value);
 				for (++it; it != x.end(); it++)
 					push_back(it.ptr->value);
@@ -101,12 +121,20 @@ namespace ft
 		~list(void) 
 		{
 			// if (!empty())
-			// 	erase(begin(), end());
+			// 	erase(begin(), end());   ////// works bad in case of 1 element
+			// delete this->tail;
 			return; 
-		};
+		}
 	
 	// *************** Overload of operator= ***************
-		list& operator= (const list& x);
+		list& operator= (const list& x)
+		{
+			erase(begin(), end());
+			iterator it = x.begin();
+			create_list_head(it.ptr->value);
+			for (++it; it != x.end(); it++)
+				push_back(it.ptr->value);
+		}
 
 	// *************** Iterators ***************
 		iterator begin(void)
@@ -115,15 +143,23 @@ namespace ft
 			return beg;
 		}
 		
-		const_iterator begin() const { return begin(); }
+		const_iterator begin() const 
+		{
+			iterator beg(this->head);
+			return beg;
+		}
 
 		iterator end(void) 
 		{
-			iterator end(this->tail->next); ////// 												need to be fixed
+			iterator end(this->tail);
 			return end;
 		}
 
-		// const_iterator end() const;
+		const_iterator end() const
+		{
+			iterator end(this->tail);
+			return end;
+		}
 
 		// reverse_iterator rbegin();
 		
@@ -146,40 +182,58 @@ namespace ft
 		
 		const_reference front(void) const { return this->head->value; }
 		
-		reference back(void) { return this->tail->value; }
+		reference back(void) { return this->tail->prev->value; }
 		
-		const_reference back(void) const { return this->tail->value; }
+		const_reference back(void) const { return this->tail->prev->value; }
 	
 	// *************** Modifiers ***************
-		template <class InputIterator>
-		void assign (InputIterator first, InputIterator last);
+		// template <class InputIterator>
+		// void assign (InputIterator first, InputIterator last)
+		// {
+		// 	InputIterator it;
+		// 	size_type size = 0;
+		// 	for (it = first; it != last; it++)
+		// 		size++;
+		// 	T temp[size];
+		// 	size_type i = 0;
+		// 	for (it = first; it != last; it++, i++)
+		// 		temp[i] = it;
+		// 	erase(begin(), end());
+		// 	for (i = 0; i < size; i++)
+		// 		push_back(temp[i]);
+		// 	return;
+		// }
 
-		void assign (size_type n, const value_type& val);
+		void assign (size_type n, const value_type& val)
+		{
+			clear();
+			for (size_type i = 0; i < n; i++)
+				push_front(val);
+			return;
+		}
 
 		void push_front (const value_type& val)
 		{
-			if (empty())
-				create_list_head(val);
-			else
-			{
-				node * current = new node;
-				current->value = val;
-				current->prev = nullptr;
-				current->next = this->head;
-				this->head->prev = current;
-				this->head = current;
-				this->list_size++;
-			}
+			node * current = new node;
+			current->value = val;
+			current->prev = this->tail;
+			current->next = this->head;
+			this->head->prev = current;
+			this->tail->next = current;
+			this->head = current;
+			this->list_size++;
+			this->tail->value = list_size;
 			return;
 		}
 
 		void pop_front(void)
 		{
 			node * current = this->head->next;
-			current->prev = nullptr;
+			current->prev = this->tail;
 			delete head;
-			head = current;
+			this->head = current;
 			this->list_size--;
+			this->tail->value = list_size;
 			return;
 		}
 
@@ -190,86 +244,109 @@ namespace ft
 			else
 			{
 				node * current = new node;
+				node * previous = this->tail->prev;
 				current->value = val;
-				current->prev = this->tail;
-				current->next = nullptr;
-				this->tail->next = current;
-				this->tail = current;
+				current->prev = previous;
+				previous->next = current;
+				current->next = this->tail;
+				this->tail->prev = current;
 				this->list_size++;
+				this->tail->value = list_size;
 			}
 			return;
 		}
 
 		void pop_back(void)
 		{
-			node * current = this->tail->prev;
-			current->next = nullptr;
-			delete this->tail;
-			this->tail = current;
+			node * current = this->tail->prev->prev;
+			current->next = this->tail;
+			delete this->tail->prev;
+			this->tail->prev = current;
 			this->list_size--;
+			this->tail->value = list_size;
 			return;
 		}
 
 		iterator insert (iterator position, const value_type& val)
 		{
-			node *new_node = new node;
-			new_node->value = val;
-			new_node->prev = position.ptr->prev;
-			new_node->next = position.ptr;
-			position.ptr->prev->next = new_node;
-			position.ptr->prev = new_node;
-			return iterator(new_node);
+			if (position == this->head)
+			{
+				push_front(val);
+				return (begin());
+			}
+			else
+			{
+				node *new_node = new node;
+				node *prev = position.ptr->prev;
+				new_node->value = val;
+				new_node->prev = prev;
+				new_node->next = position.ptr;
+				prev->next = new_node;
+				position.ptr->prev = new_node;
+				this->list_size++;
+				this->tail->value = list_size;
+				return iterator(new_node);
+			}
 		}
 
-		void insert (iterator position, size_type n, const value_type& val);
+		void insert (iterator position, size_type n, const value_type& val)
+		{
+			for (size_type i = 0; i < n; i++)
+				insert(position, val);
+			return;
+		}
 
-		template <class InputIterator>
-    	void insert (iterator position, InputIterator first, InputIterator last);
+		// template <class InputIterator>
+		// void insert (iterator position, InputIterator first, InputIterator last)
+		// {
+		// 	InputIterator it;
+		// 	for (it = first; it != last; it++)
+		// 		insert(position, it);
+		// 	return;
+		// }
 
 		iterator erase (iterator position)
 		{
-			if (position != end())
+			iterator temp = position++;
+			position--;
+			if (position.ptr == this->head)
+				pop_front();
+			else
 			{
-				iterator temp = end();
-				if (position.ptr->prev == nullptr)
-				{
-					this->head = position.ptr->next;
-					this->head->prev = nullptr;
-					temp = position++;
-				}
-				else if (position.ptr->next == nullptr)
-				{
-					this->tail = position.ptr->prev;
-					this->tail->next = nullptr;
-				}
-				else
-				{
-					position.ptr->prev->next = position.ptr->next;
-					position.ptr->next->prev = position.ptr->prev;
-					temp = position++;
-				}
+				position.ptr->prev->next = position.ptr->next;
+				position.ptr->next->prev = position.ptr->prev;
 				delete position.ptr;
+				position.ptr = nullptr;
 				this->list_size--;
-				return temp;
+				this->tail->value = list_size;
 			}
-			return position;
+			return temp;
 		}
 
 		iterator erase (iterator first, iterator last)
 		{
 			iterator i;
-			i = first;
 			
-			while (i != last)
-				i = erase(i);
+			for (i = first; i != last; i++)
+				erase(i);
 			return last;
 		}
 		
-		void swap (list& x);
+		// void swap (list& x)
+		// {
+		// 	ft::list<T> temp(x);
+		// 	x.assign(this->begin(), this->end());
+		// 	assign(temp.begin(), temp.end());
+		// 	return;
+		// }
 
 		void resize (size_type n, value_type val = value_type());
 
-		void clear(void);
+		void clear(void)
+		{
+			erase(begin(), end());
+			return;
+		}
 
 	// *************** Operations ***************
 		void splice (iterator position, list& x);
